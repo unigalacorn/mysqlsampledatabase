3ddd0faf01d0b661267de8848bdfc900c1bbfb4b
unigalacorn <60454853+unigalacorn@users.noreply.github.com>
2020-03-12 10:59:05 +0800

    Update vieworder.php

--- vieworder.php
+++ vieworder.php
@@ -39,19 +39,19 @@
 	   
        <table cellpadding="10"> <tr> <thead>
        
-		 <th>Customer No. </th>
-       	  <th>Customer Name </th>
-       	  <th>Contact Last Name </th>
-       	  <th>Contact First Name </th>
+		  <th>CNo. </th>
+       	  <th>CName </th>
+       	  <th>ConLName </th>
+       	  <th>ConFName </th>
 		  <th>Phone </th>
-       	  <th>AddressLine1 </th>
-		  <th>AddressLine2 </th>
+       	  <th>Addr1 </th>
+		  <th>Addr2 </th>
        	  <th>City </th>
 		  <th>State </th>
-		  <th>Postal Code </th>
+		  <th>PosCode </th>
 		  <th>Country </th>
-		  <th>Sales Rep No. </th>
-		  <th>Credit Limit </th>
+		  <th>SalesRepNo. </th>
+		  <th>CredLim </th>
        	 
        </thead> </tr>
 
@@ -95,10 +95,82 @@
 			
 		</td> </center>
 
-	
+			
+<?php
+       $servername = "localhost";
+       $username = "root";
+       $password = "";
+       $dbname = "classicmodels";
+
+	   
+       $conn = new mysqli($servername, $username, $password, $dbname);
+      
+       if ($conn->connect_error) {
+		   
+         die("Connection failed: " . $conn->connect_error);
+		 
+       } 
+	   
+       $sql = "SELECT * from employees";
+       $result = $conn->query($sql); 
+
+?> 	
+	<br> <br> <br> <br> <br> <br> <br>
+   <center>
+   <div class="table-users">
+   <div class="header"><font size=5> Employees </font></div>
+   <table cellspacing="0">
+   <br>
+	   
+       <table cellpadding="10"> <tr> <thead>
+       
+		  <th>EmpNo.</th>
+		  <th>LName</th>
+		  <th>FName</th>
+		  <th>Ext</th>
+		  <th>Email</th>
+		  <th>OffCode</th>
+		  <th>RepTo</th>
+		  <th>JobTit</th>
+		  
+       </thead> </tr>
+
+<?php
+
+		if ($result->num_rows > 0) {
+			//
+        while($row = $result->fetch_assoc()) { 
+		
+?>			
+            <tr>
+			<td ><?php echo $row['employeeNumber'] ?></td> 
+			<td><?php echo $row['lastName'] ?></td>
+			<td><?php echo $row['firstName'] ?></td>
+			<td><?php echo $row['extension'] ?></td>
+			<td><?php echo $row['email'] ?></td>
+			<td><?php echo $row['officeCode'] ?></td>
+			<td><?php echo $row['reportsTo'] ?></td>
+			<td><?php echo $row['jobTitle'] ?></td>
+            </tr>	 
+			
+			
+<?php 
+        }
+		
+  	    }else echo "<script type=\"text/javascript\">window.alert('Data Base Empty');
+					window.location.href = '/lab01/Welcome.html';</script>"; ; 
+        $conn->close(); 
+    
+?>
+		 <table> <center> <td> 
+			
+		</center>
+    </form>
+			
+		</td> </center>
 
 	
-
+		
 	
 <?php
        $servername = "localhost";
